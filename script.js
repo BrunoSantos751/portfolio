15b3c16910f439325ee331edcbf1ff2160224347
Brunosantos751 <brunomoraesleao@hotmail.com>
2026-02-12 20:07:44 -0300

    estilo adicionado

---
+++ script.js
@@ -0,0 +1,21 @@
+// DARK MODE
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+document.body.classList.toggle("light");
+toggle.textContent =
+document.body.classList.contains("light") ? "🌙" : "☀️";
+});
+
+// ANIMAÇÃO AO SCROLL
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+if (entry.isIntersecting) {
+entry.target.classList.add("show");
+}
+});
+});
+
+elements.forEach(el => observer.observe(el));
