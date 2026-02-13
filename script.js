a7242aab0567aee2c0be835d52035fc3139dc636
Brunosantos751 <brunomoraesleao@hotmail.com>
2026-02-12 22:30:54 -0300

    refactor: clean up HTML structure and improve CSS formatting

--- script.js
+++ script.js
@@ -2,20 +2,19 @@
 const toggle = document.getElementById("theme-toggle");
 
 toggle.addEventListener("click", () => {
-document.body.classList.toggle("light");
-toggle.textContent =
-document.body.classList.contains("light") ? "🌙" : "☀️";
+  document.body.classList.toggle("light");
+  toggle.textContent = document.body.classList.contains("light") ? "🌙" : "☀️";
 });
 
 // ANIMAÇÃO AO SCROLL
 const elements = document.querySelectorAll(".fade-in");
 
 const observer = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-if (entry.isIntersecting) {
-entry.target.classList.add("show");
-}
-});
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
 });
 
 elements.forEach(el => observer.observe(el));
